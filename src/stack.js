869ec77dffc321ae7c629f4ea1700bd201f7f924
Minchuk Ilya <cacatubutyt@gmail.com>
2023-04-16 12:43:52 +0200

    feat: solve stack task

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+
 /**
  * Implement the Stack with a given interface via array.
  *
@@ -13,19 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.stack = []
+  }
+
+  push(element) {
+    this.stack.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const removedElement = this.stack.pop()
+    return removedElement
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1]
   }
 }
 
